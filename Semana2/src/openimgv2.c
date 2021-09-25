01506297b1deb3366cc953b0cad47b20652720ba
Manuel Fresneda Llamas <manololabestiaoficial@hotmail.com>
2021-09-25 16:09:36 +0200

    Errores

--- Semana2/src/openimgv2.c
+++ Semana2/src/openimgv2.c
@@ -65,9 +65,9 @@ int main(int argc, char **argv){
     }
     //necesito un buffer donde guardar los PID y esperar a que todos los PID terminen
     
-    for(int i=3;i<argc-1;i++){
+    for(int i=3;i<argc;i++){
        // arrayPID[i-3]=fork(); //creamos proceso hijo
-        
+        //lfmaimo@um.es
         switch (pid=fork()){
             case -1: //error de fork
                 perror("fork():"); //perror se usa cuando son errores de llamadas al sistema
@@ -80,21 +80,22 @@ int main(int argc, char **argv){
                     fprintf(stderr,"No deberia entrar aqui\n");
                     exit(EXIT_FAILURE);
                 }
-                if(execlp(argVisor,argVisor,argv[i],NULL)==-1){
+                fprintf(stderr,"jajajajaj\n");
+                execlp("pepe","pepe", "noexiste",NULL);
                     fprintf(stderr,"Error: '%s' no encontrado\n",argVisor);
                     exit(EXIT_FAILURE);
                     break;
-                }
+                
 
             default: //lo que hace el padre
                 arrayPID[i-3]=pid;
                 //for(int a=0;a<3;a++){
                     //if (waitpid(arrayPID[a],&status,0)==-1 && waitpid(arrayPID[a+1],&status,0)==-1 && waitpid(arrayPID[a+2],&status,0)==-1){ /* Espera a que termine el proceso hijo */ 
-                    if(waitpid(arrayPID[i-3],&status,WUNTRACED)){
-                        fprintf(stderr,"No deberia entrar aqui aun\n");
-                        perror("waitpid()");
-                        //exit(EXIT_FAILURE);
-                    }
+                    // if(waitpid(arrayPID[i-3],&status,WUNTRACED)){
+                    //     fprintf(stderr,"No deberia entrar aqui aun\n");
+                    //     perror("waitpid()");
+                    //     //exit(EXIT_FAILURE);
+                    // }
         }
         
     }
