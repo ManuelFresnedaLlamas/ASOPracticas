39529e8519e58eb262db9038dd80f88e8dc69e3b
Manuel Fresneda Llamas <manololabestiaoficial@hotmail.com>
2021-09-30 17:21:37 +0200

    Añadida funcion getopt, tratamiento del numero de ficheros y apertura de descriptor de fichero segun sea null o no

--- Semana3/src/Tarea3/merge_files.c
+++ Semana3/src/Tarea3/merge_files.c
@@ -7,9 +7,104 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <ctype.h>
+
+
+void imprimirUso(void){
+    fprintf(stderr, "Uso: merge_files [-t BUFSIZE] [-o FILEOUT] FILEIN1 [FILEIN2 ...FILEINn]\n");
+}
+void imprimirInstrucciones(void){
+    
+    fprintf(stderr, "NO admite lectura de la entrada estandar.\n");
+    fprintf(stderr, "-t BUFSIZE Tamaño de buffer donde 1<= BUFSIZE <=128MB\n");
+    fprintf(stderr, "-o FILEOUT Usa FILEOUT en lugar de la salida estandar\n");
+        
+}
 
 int main(int argc, char **argv)
 {
+    int opt;
+    int fdout;
+    int aux=0; //variable que usaremos para recoger el char* de -t
+    char * memReservar=NULL;
+    char * ficheroSalida=NULL;
+    
+    if (argc == 1)
+    {
+        fprintf(stderr, "Error: No hay ficheros de entrada.\n");
+        imprimirUso();
+        imprimirInstrucciones();
+        exit(EXIT_FAILURE);
+    }
+
+    while ((opt = getopt(argc, argv, "tn:on")) != -1)
+    {
+        //fprintf(stdout,"%d",*optarg);
+        switch (opt)
+        {
+        case 't': //isdigit -> devuelve 0 si no es un caracter numérico
+            memReservar=optarg;
+            break;
+        case 'o':
+            ficheroSalida=optarg;
+            break;
+
+        //default:
+            //Se tendra buffer de 1024 bytes y FILEOUT será la salida estándar
+
+            
+        }
+    }
+    //Comprobamos que no se superen 16 ficheros.
+    int contadorAux=0;
+    //si memRe!=NULL -> argc [1] y argc[2] llenos
+    //si ficheroSalida tambien != NULL -> argc[3] y argc[4]
+    if(memReservar!=NULL && ficheroSalida!=NULL){
+        for(int i=5;i<argc;i++){
+            contadorAux++;
+            if(contadorAux>16){ //si tenemos más de 16 ficheros
+                fprintf(stderr,"Error: Demasiados ficheros de entrada. Máximo 16 ficheros.\n");
+                imprimirInstrucciones();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    //si solo uno de ambos tiene NULL -> arg [1] y [2] ocupados
+    if(memReservar!=NULL || ficheroSalida!=NULL){
+        for(int i=3;i<argc;i++){
+            contadorAux++;
+            if(contadorAux>16){ //si tenemos más de 16 ficheros
+                fprintf(stderr,"Error: Demasiados ficheros de entrada. Máximo 16 ficheros.\n");
+                imprimirInstrucciones();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    if(memReservar==NULL && ficheroSalida==NULL){
+        for(int i=1;i<argc;i++){
+            contadorAux++;
+            if(contadorAux>16){
+                fprintf(stderr,"Error: Demasiados ficheros de entrada. Máximo 16 ficheros.\n");
+                imprimirInstrucciones();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    //Si se da fichero salida, abrimos y guardamos su fd
+    if (ficheroSalida != NULL)
+    {
+        fdout = open(ficheroSalida, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (fdout == -1)
+        {
+            perror("open(fileout)");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else /* Por defecto, la salida estándar */
+        fdout = STDOUT_FILENO;
+
 
 
 }
